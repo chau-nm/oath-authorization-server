5d12deb98d6a290ce98c5e273057ca7094c9428b
chau-nm <chaunm.146.2001@gmail.com>
2024-10-05 14:18:58 +0700

    init

---
+++ src/test/java/oauth/authorization/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package oauth.authorization.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
